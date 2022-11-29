b2723464f8187c3a8f99413c44b4a39a28f69f0a
Dark Tornado <dt3141592@naver.com>
2022-11-30 06:39:38 +0900

    init

---
+++ back-end/index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+http.createServer(async (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8;',
+        'Access-Control-Allow-Origin': '*'
+    });
+    res.write("test");
+    res.end();
+}).listen(8080);
