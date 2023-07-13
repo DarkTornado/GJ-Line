66b34ef061dcfbc106f24e94ed347aa572f79037
Dark Tornado <dt3141592@naver.com>
2023-07-14 00:14:08 +0900

    변동사항 반영

--- script.js
+++ script.js
@@ -57,7 +57,21 @@ var pos = [
 ];
 
 
+var tick = 0, touch = 120;
+function secondTick() {
+	if (tick > 0) {
+		tick--;
+	}
+	if (tick == 0) {
+		if (touch > 0) update();
+		else changeUI('none', 'inline-block');
+	}
+	if (touch > 0) {
+		touch--;
+	}
+}
 function update() {
+	tick = 20;
     fetch('https://api.darktornado.net/subway/gj-line')
         .then((response) => response.text())
         .then((data) => {
@@ -65,18 +79,51 @@ function update() {
         });
 }
 update();
+setInterval(secondTick, 1000);
+document.body.addEventListener('click', (e) => {
+	if (touch == 0) changeUI('block', 'none');
+    touch = 120;
+});
+document.body.addEventListener('touchstart', (e) => {
+	if (touch == 0) changeUI('block', 'none');
+    touch = 120;
+});
 
+function changeUI(map, alert) {
+	document.querySelector('div#map_area').style['display'] = map;
+	document.querySelector('div#alert_area').style['display'] = alert;
+}
+
+var map = null;
 function applyData(data) {
     data = JSON.parse(data);
-    const map = document.getElementById("map").innerHTML;
 
-    var src = '';
-    data.forEach((e, i) => {
-        e.sta
-        src += train(pos[i].x, pos[i].y, pos[i].dir, e.up, true);
-        src += train(pos[i].x, pos[i].y, pos[i].dir, e.down, false);
-    });
-    document.getElementById("map").innerHTML = map + src;
+    if (window.innerWidth >= 768) {  //PC에서 접속한 경우
+        var src = '';
+        if (map == null) map = document.getElementById("map").innerHTML;
+        data.forEach((e, i) => {
+            src += train(pos[i].x, pos[i].y, pos[i].dir, e.up, true);
+            src += train(pos[i].x, pos[i].y, pos[i].dir, e.down, false);
+        });
+        document.getElementById("map").innerHTML = map + src;
+    } else {
+        var src = '<svg id="map_mobile" width=100% viewBox="0 0 600 5400">';
+        src += '<polyline points="100,100 100,5300" fill="none" stroke="#77C4A3" />';
+        src += '<polyline points="100,2000 150,2050 350,2050 400,2100 400,2200" fill="none" stroke="#77C4A3" />';
+        var seoul = [data.pop()];
+        seoul.unshift(data.pop());
+        data.forEach((e, i) => {  //모바일에서 접속한 경우
+            var y = (i + 1) * 100;
+            src += station(100, y, e.station);
+            src += train_up(35, y, e.up);
+            src += train_down(65, y, e.down);
+        });
+        src += station(400, 2100, seoul[0].station) + train_up(335, 2100, seoul[0].up) + train_down(365, 2100, seoul[0].down);
+        src += station(400, 2200, seoul[1].station) + train_up(335, 2200, seoul[1].up) + train_down(365, 2200, seoul[1].down);
+        src += '</svg>';
+        document.getElementById("map_mobile").innerHTML = src;
+    }
+    console.log('updated');
 }
 
 function train(x, y, dir, type, isUp) {
@@ -91,6 +138,24 @@ function train(x, y, dir, type, isUp) {
     return "<image xlink:href='images/" + icons[type] + ".svg' x='" + x + "' y='" + y + "' width='40px' transform='rotate(" + dirs[dir] + "," + (x + 20) + "," + (y + 20) + ")'/>";
 }
 
+function station(x, y, sta) {
+    return "<circle cx='"+x+"' cy='"+y+"' r='13' /><text x="+(x+50)+" y="+y+">"+sta+"</text>";
+}
+
+function train_up(x, y, type) {
+    var icons = ['no_train', 'train'];
+    x += 15;
+    y -= 20;
+    return "<image xlink:href='images/" + icons[type] + ".svg' x='" + x + "' y='" + y + "' width='40px'/>";
+}
+
+function train_down(x, y, type) {
+    var icons = ['no_train', 'train'];
+    x += 45;
+    y -= 20;
+    return "<image xlink:href='images/" + icons[type] + ".svg' x='" + x + "' y='" + y + "' width='40px' transform='rotate(180," + (x + 20) + "," + (y + 20) + ")'/>";
+}
+
 
 function onIconClicked(station) {
     alert('아이콘: ' + station);
@@ -99,3 +164,4 @@ function onIconClicked(station) {
 function onTextClicked(element) {
     alert('글자: ' + element.innerHTML.replace(/(<([^>]+)>)/g, ''));
 }
+
