630e53fc6b1b25b1f712d209a0936ae9f32f235f
Dark Tornado <dt3141592@naver.com>
2025-12-02 07:46:56 +0900

    Update script.js

--- front-end/script.js
+++ front-end/script.js
@@ -14,7 +14,8 @@ function secondTick() {
 var currentData = null;
 function update() {
     tick = 20;
-    fetch('back-end url')
+    fetch('https://api.darktornado.net/subway/gj-line')
+    //fetch('back-end url')
         .then((response) => response.text())
         .then((data) => {
             applyData(data);
@@ -46,13 +47,13 @@ function applyData(data) {
 var MAP = null;
 
 function updatePC(data) {
-	var map = document.getElementById("map");
+    var map = document.getElementById("map");
     if (MAP == null) MAP = map.innerHTML;
-	
+    
     var src = '';
     data.forEach((e, i) => {
-        src += train(STNS[i].x, STNS[i].y, STNS[i].dir, e.up, true);
-        src += train(STNS[i].x, STNS[i].y, STNS[i].dir, e.down, false);
+        src += train(STNS[i], e.up, true);
+        src += train(STNS[i], e.down, false);
     });
     map.innerHTML = MAP + src;
 }
@@ -66,55 +67,76 @@ function updateMobile(data) {
     data.forEach((e, i) => {
         var y = (i + 1) * 100;
         src += station(100, y, e.station);
-        src += train_up(35, y, e.up);
-        src += train_down(65, y, e.down);
+        src += train_up(35, y, e.up, e.station);
+        src += train_down(65, y, e.down, e.station);
     });
-    src += station(400, 2300, seoul[0].station) + train_up(335, 2300, seoul[0].up) + train_down(365, 2300, seoul[0].down);
-    src += station(400, 2400, seoul[1].station) + train_up(335, 2400, seoul[1].up) + train_down(365, 2400, seoul[1].down);
+    src += station(400, 2300, seoul[0].station) + train_up(335, 2300, seoul[0].up, seoul[0].station) + train_down(365, 2300, seoul[0].down, seoul[0].station);
+    src += station(400, 2400, seoul[1].station) + train_up(335, 2400, seoul[1].up, seoul[1].station) + train_down(365, 2400, seoul[1].down, seoul[1].station);
     src += '</svg>';
     document.getElementById("map_mobile").innerHTML = src;
 }
 
-function train(x, y, dir, train, isUp) {
+function train(stn, train, isUp) {
+    var dir = stn.dir;
     if (!isUp) dir = (dir + 1) % 2;
     var dirs = [-90, 90];
     var dx = [-30, -10];
     var dy = [20, -60];
-    x = x + dx[dir];
-    y = y + dy[dir];
+    var x = stn.x + dx[dir];
+    var y = stn.y + dy[dir];
     var dx = [32, 8];
     var dy = [12, 32];
     var tx = x + dx[dir];
     var ty = y + dy[dir];
-    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px' transform='rotate(" + dirs[dir] + "," + (x + 20) + "," + (y + 20) + ")'/>"
-    if (train.length > 0) result += '<text class=train_pc x='+tx+' y='+ty+' fill=#BDBDBD>'+terminal(train[0].terminal)+'</text>';
+    var str = [];
+    train.forEach((e, i) => {
+        str.push(e.terminal + (e.isExpress?'행 급행':'행 ') + '열차 ' + stn.stn + ' ' + e.status);
+    });
+    str = str.join(',');
+    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px' transform='rotate(" + dirs[dir] + "," + (x + 20) + "," + (y + 20) + ")' onclick=\'showTrain(\""+str+"\");\' />"
+    if (train.length > 0) result += '<text class=train_pc x='+tx+' y='+ty+' fill=#BDBDBD onclick=\'showTrain(\"'+str+'\");\' >'+terminal(train[0].terminal)+'</text>';
     return result;
+}
+
+function showTrain(str) {
+    if (str == '') str += '열차 없음';
+    alert(str.replace(/,/g, '\n'));
 }
 
 function terminal(term) {
-if (term == undefined) return '';
-if (term.length > 4) return term.slice(0, 3)+'..';
-return term;
+    if (term == undefined) return '';
+    if (term.length > 4) return term.slice(0, 3)+'..';
+    return term;
 }
 
 
-function station(x, y, sta) {
-    return "<circle cx='" + x + "' cy='" + y + "' r='13' /><text x=" + (x + 50) + " y=" + y + " onclick=showTrainInfo('" + sta + "');>" + sta + "</text>";
+function station(x, y, stn) {
+    return "<circle cx='" + x + "' cy='" + y + "' r='13' /><text x=" + (x + 50) + " y=" + y + ">" + stn + "</text>";
 }
 
-function train_up(x, y, train) {
+function train_up(x, y, train, stn) {
     x += 5;
     y -= 30;
-    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px'/>";
-    if (train.length > 0) result += '<text class=train_mobile x='+(x+20)+' y='+(y+20)+' fill=#BDBDBD transform="rotate(90,' + (x + 20) + "," + (y + 30) + ')">'+terminal(train[0].terminal)+'</text>';
+    var str = [];
+    train.forEach((e, i) => {
+        str.push(e.terminal + (e.isExpress?'행 급행':'행 ') + '열차 ' + stn + ' ' + e.status);
+    });
+    str = str.join(',');
+    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px' onclick=\'showTrain(\""+str+"\");\' />";
+    if (train.length > 0) result += '<text class=train_mobile x='+(x+20)+' y='+(y+20)+' fill=#BDBDBD transform="rotate(90,' + (x + 20) + "," + (y + 30) + ')"  onclick=\'showTrain(\"'+str+'\");\' >'+terminal(train[0].terminal)+'</text>';
     return result;
 }
 
-function train_down(x, y, train) {
+function train_down(x, y, train, stn) {
     x += 55;
     y -= 10;
-    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px' transform='rotate(180," + (x + 20) + "," + (y + 20) + ")'/>";
-    if (train.length > 0) result += '<text class=train_mobile x='+(x+20)+' y='+(y+15)+' fill=#BDBDBD transform="rotate(90,' + (x + 20) + "," + (y + 5) + ')">'+terminal(train[0].terminal)+'</text>';
+    var str = [];
+    train.forEach((e, i) => {
+        str.push(e.terminal + (e.isExpress?'행 급행':'행 ') + '열차 ' + stn + ' ' + e.status);
+    });
+    str = str.join(',');
+    var result = "<image xlink:href='images/" + getIcon(train) + ".svg' x='" + x + "' y='" + y + "' width='60px' transform='rotate(180," + (x + 20) + "," + (y + 20) + ")'onclick=\'showTrain(\""+str+"\");\'  />";
+    if (train.length > 0) result += '<text class=train_mobile x='+(x+20)+' y='+(y+15)+' fill=#BDBDBD transform="rotate(90,' + (x + 20) + "," + (y + 5) + ')" onclick=\'showTrain(\"'+str+'\");\' >'+terminal(train[0].terminal)+'</text>';
     return result;
 }
 
@@ -129,31 +151,5 @@ function getIcon(train) {
     if (exp && all) return 'trains';
     if (exp && !all) return 'express_train';
     return 'train';
-}
-
-function showTrainInfo(station) {
-    if (currentData == null) return;
-    currentData.forEach((e) => {
-        if (e.station != station) return;
-        var trains = [];
-        if (e.up.length > 0) e.up.forEach((e) => {
-            trains.push(e.terminal + (e.isExpress?'급':'') + '행 열차 '+ station + ' ' + e.status);
-        })
-        if (e.down.length > 0) e.down.forEach((e) => {
-            trains.push(e.terminal + (e.isExpress?'급':'') + '행 열차 '+ station + ' ' + e.status);
-        })
-        if (trains.length == 0) alert('해당 역에는 열차가 없어요');
-        else alert(trains.join('\n'));
-    })
-}
-
-function onIconClicked(station) {
-    //alert('아이콘: ' + station);
-    showTrainInfo(station); //열차 아이콘의 투명한 부분이 원을 가려서 클릭 불가능
-}
-
-function onTextClicked(station) {
-    //alert('글자: ' + station);
-    showTrainInfo(station);
 }
 
